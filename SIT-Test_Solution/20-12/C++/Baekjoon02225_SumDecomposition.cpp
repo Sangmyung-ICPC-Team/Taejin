bb88d5bfc36e20de8aeb76d94c5b05e39a5bb3df
Jin-MacBook <taejin7824@gmail.com>
2020-12-31 05:14:14 +0900

    BOJ 2225 Many Solutions

--- SIT-Test_Solution/20-12/C++/Baekjoon02225_SumDecomposition.cpp
+++ SIT-Test_Solution/20-12/C++/Baekjoon02225_SumDecomposition.cpp
@@ -10,15 +10,34 @@ int main(void) {
 	int N, K;
 	cin >> N >> K;
 
-	long long dp[201][201] = { 0LL, };
+	int dp[201][201] = { 0, };
 	for ( int i = 1; i <= K; i++ )
 		dp[0][i] = 1;
 
+	// Solution 1: Sum of All
 	for ( int i = 1; i <= N; i++ ) {
 		dp[i][1] = 1;
 		for ( int j = 2; j <= K; j++ )
 			for ( int k = 0; k <= i; k++ )
 				dp[i][j] =  (dp[i][j] + dp[k][j - 1]) % MOD;
+	}
+
+	cout << dp[N][K] << '\n';
+
+	// Solution 2: Using prefix sum
+	for ( int i = 1; i <= N; i++ ) {
+		dp[i][1] = dp[i - 1][1] + 1;
+		for ( int j = 2; j <= K; j++ )
+			dp[i][j] = ( dp[i - 1][j] + dp[i][j - 1] ) % MOD;
+	}
+
+	cout << (dp[N][K] - dp[N - 1][K] + MOD) % MOD << '\n';
+
+	// Solution 3: Using value caculated already
+	for ( int i = 1; i <= N; i++ ) {
+		dp[i][1] = 1;
+		for ( int j = 2; j <= K; j++ )
+			dp[i][j] = ( dp[i - 1][j] + dp[i][j - 1] ) % MOD;
 	}
 
 	cout << dp[N][K] << '\n';
